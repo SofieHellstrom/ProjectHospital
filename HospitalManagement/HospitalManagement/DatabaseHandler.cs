532d6de1b786fb8f5d87c621550613bab2516af3
Johannes Svensson <mirrorspawn@gmail.com>
2017-10-03 09:35:43 +0200

    Reverted back to earlier version, then put back the Patient, Employee and DatabaseHandler classes.

---
+++ HospitalManagement/HospitalManagement/DatabaseHandler.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+using NpgsqlTypes;
+
+namespace HospitalManagement
+{
+    public  class DatabaseHandler
+    {
+        private string ConnectionString = "Host=localhost;Username=postgres;Password=projekt;Database=Hospital";
+
+        public bool PatientExists(Patient patientToSearchFor)
+        {
+            using (var conn = new NpgsqlConnection(ConnectionString))
+            {
+                conn.Open();
+                using (var cmd = new NpgsqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = "SELECT COUNT(person_id_nr) FROM patient WHERE person_id_nr = :id";
+
+                    cmd.Parameters.Add(new NpgsqlParameter("id", NpgsqlDbType.Varchar));
+
+                    cmd.Prepare();
+
+                    cmd.Parameters[0].Value = patientToSearchFor.Personnummer;
+
+                    int result = cmd.ExecuteNonQuery();
+                    return Convert.ToBoolean(result);
+                }
+
+            }
+
+        }
+
+        public List<Patient> LoadAllPatients()
+        {
+            //Returns a new instance of a specific person based on the value of person_id_nr 
+            //in the patient table of the database.
+            List<Patient> resultList = new List<Patient>();
+            string postOrt;
+            using (var conn = new NpgsqlConnection(ConnectionString))
+            {
+                conn.Open();
+
+                using (var cmd = new NpgsqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = "SELECT * FROM patient";
+
+                    cmd.Parameters.Add(new NpgsqlParameter("id", NpgsqlDbType.Varchar));
+
+                    cmd.Prepare();
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        string personNr;
+                        string firstName;
+                        string lastName;
+                        string adress;
+                        int postNr;
+                        string telefonNr;
+                        string eMail;
+                        string blodTyp;
+                        
+                        while (reader.Read())
+                        {
+                            personNr = reader.GetString(0);
+                            firstName = reader.GetString(1);
+                            lastName = reader.GetString(2);
+                            adress = reader.GetString(3);
+                            postNr = reader.GetInt32(4);
+                            telefonNr = reader.GetString(5);
+                            eMail = reader.GetString(6);
+                            blodTyp = reader.GetString(7);
+
+                            postOrt = loadPostort(postNr);
+
+                            Patient patientToAdd = new Patient(personNr, firstName, lastName, adress, postNr, postOrt, telefonNr, eMail, blodTyp);
+                            resultList.Add(patientToAdd);
+                        }
+                        return resultList;
+                        
+                    }
+                }
+                
+            }
+            
+
+
+        }
+
+        public Patient LoadPatient(string personnr)
+        {
+            //Returns a new instance of a specific person based on the value of person_id_nr 
+            //in the patient table of the database.
+            Patient returnPatient;
+            string personNr;
+            string firstName;
+            string lastName;
+            string adress;
+            int postNr;
+            string telefonNr;
+            string eMail;
+            string blodTyp;
+            string postOrt;
+
+            using (var conn = new NpgsqlConnection(ConnectionString))
+            {
+                conn.Open();
+                using (var cmd = new NpgsqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = "SELECT * FROM patient WHERE person_id_nr = :id";
+
+                    cmd.Parameters.Add(new NpgsqlParameter("id", NpgsqlDbType.Varchar));
+
+                    cmd.Prepare();
+
+                    cmd.Parameters[0].Value = personnr;
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        personNr = reader.GetString(0);
+                        firstName = reader.GetString(1);
+                        lastName = reader.GetString(2);
+                        adress = reader.GetString(3);
+                        postNr = reader.GetInt32(4);
+                        telefonNr = reader.GetString(5);
+                        eMail = reader.GetString(6);
+                        blodTyp = reader.GetString(7);
+                    }
+                }
+
+                postOrt = loadPostort(postNr);
+                returnPatient = new Patient(personNr, firstName, lastName, adress, postNr, postOrt, telefonNr, eMail, blodTyp);
+                return returnPatient;
+            }
+        }
+
+        public bool AddPatient(Patient patientToAdd)
+        {
+            //Adds a new patient to the database. As of now there is no try/catch, no check if the patient already exists, and no roomnr
+            //Room is unlikely to exist when patient is created, but the rest should probably be added, either here or where the method is called from.
+
+            using (var conn = new NpgsqlConnection(ConnectionString))
+            {
+                conn.Open();
+                using (var cmd = new NpgsqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = "INSERT INTO patient (person_id_nr, first_name, last_name, address, postal_code, phone, email, bloodtype) VALUES (:id, :firstname, :lastname, :address, :postcode, :phone, :email, :blood)";
+                    
+                    cmd.Parameters.Add(new NpgsqlParameter("id", NpgsqlDbType.Varchar));
+                    cmd.Parameters.Add(new NpgsqlParameter("firstname", NpgsqlDbType.Varchar));
+                    cmd.Parameters.Add(new NpgsqlParameter("lastname", NpgsqlDbType.Varchar));
+                    cmd.Parameters.Add(new NpgsqlParameter("address", NpgsqlDbType.Varchar));
+                    cmd.Parameters.Add(new NpgsqlParameter("postcode", NpgsqlDbType.Varchar));
+                    cmd.Parameters.Add(new NpgsqlParameter("phone", NpgsqlDbType.Varchar));
+                    cmd.Parameters.Add(new NpgsqlParameter("email", NpgsqlDbType.Varchar));
+                    cmd.Parameters.Add(new NpgsqlParameter("blood", NpgsqlDbType.Varchar));
+
+                    cmd.Prepare();
+
+                    cmd.Parameters[0].Value = patientToAdd.Personnummer;
+                    cmd.Parameters[1].Value = patientToAdd.FirstName;
+                    cmd.Parameters[2].Value = patientToAdd.LastName;
+                    cmd.Parameters[3].Value = patientToAdd.Address;
+                    cmd.Parameters[4].Value = patientToAdd.PostalCode;
+                    cmd.Parameters[5].Value = patientToAdd.PhoneNr;
+                    cmd.Parameters[6].Value = patientToAdd.Email;
+                    cmd.Parameters[7].Value = patientToAdd.BloodType;
+
+                    int recordsAffected = cmd.ExecuteNonQuery();
+                    return Convert.ToBoolean(recordsAffected); //returns 1 if there were any columns affected and 0 if there wasn't. 
+
+                }
+
+            }
+        }
+
+        public bool UpdatePatient(Patient patientToUpdate)
+        {
+            using (var conn = new NpgsqlConnection(ConnectionString))
+            {
+                conn.Open();
+                using (var cmd = new NpgsqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = "UPDATE patient SET first_name = :firstname, last_name = :lastname, address = :adress, postal_code = :postcode, phone = :phone, email = :email, bloodtype = :bloodtype WHERE person_id_nr = :id";
+
+                    cmd.Parameters.Add(new NpgsqlParameter("id", NpgsqlDbType.Varchar));
+                    cmd.Parameters.Add(new NpgsqlParameter("firstname", NpgsqlDbType.Varchar));
+                    cmd.Parameters.Add(new NpgsqlParameter("lastname", NpgsqlDbType.Varchar));
+                    cmd.Parameters.Add(new NpgsqlParameter("address", NpgsqlDbType.Varchar));
+                    cmd.Parameters.Add(new NpgsqlParameter("postcode", NpgsqlDbType.Varchar));
+                    cmd.Parameters.Add(new NpgsqlParameter("phone", NpgsqlDbType.Varchar));
+                    cmd.Parameters.Add(new NpgsqlParameter("email", NpgsqlDbType.Varchar));
+                    cmd.Parameters.Add(new NpgsqlParameter("blood", NpgsqlDbType.Varchar));
+
+                    cmd.Prepare();
+
+                    cmd.Parameters[0].Value = patientToUpdate.Personnummer;
+                    cmd.Parameters[1].Value = patientToUpdate.FirstName;
+                    cmd.Parameters[2].Value = patientToUpdate.LastName;
+                    cmd.Parameters[3].Value = patientToUpdate.Address;
+                    cmd.Parameters[4].Value = patientToUpdate.PostalCode;
+                    cmd.Parameters[5].Value = patientToUpdate.PhoneNr;
+                    cmd.Parameters[6].Value = patientToUpdate.Email;
+                    cmd.Parameters[7].Value = patientToUpdate.BloodType;
+
+                    int recordsAffected = cmd.ExecuteNonQuery();
+                    return Convert.ToBoolean(recordsAffected); //returns 1 if there were any columns affected and 0 if there wasn't. 
+
+                }
+
+            }
+        }
+
+        public bool DeletePatient(string patientToDelete)
+        {
+            using (var conn = new NpgsqlConnection(ConnectionString))
+            {
+                conn.Open();
+                using (var cmd = new NpgsqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = "DELETE FROM patient WHERE person_id_nr = :id";
+
+                    cmd.Parameters.Add(new NpgsqlParameter("id", NpgsqlDbType.Varchar));
+                    
+                    cmd.Prepare();
+
+                    cmd.Parameters[0].Value = patientToDelete;
+                    
+                    int recordsAffected = cmd.ExecuteNonQuery();
+                    return Convert.ToBoolean(recordsAffected); //returns 1 if there were any columns affected and 0 if there wasn't. 
+
+                }
+
+            }
+        }
+
+        public string loadPostort(int postkod)
+        {
+            string returnPostOrt;
+            using (var conn = new NpgsqlConnection(ConnectionString))
+            {             
+                conn.Open();
+                using (var cmd = new NpgsqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = "SELECT placename FROM place WHERE postal_code = :code";
+
+                    cmd.Parameters.Add(new NpgsqlParameter("code", NpgsqlDbType.Integer));
+
+                    cmd.Prepare();
+
+                    cmd.Parameters[0].Value = postkod;
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        returnPostOrt = reader.GetString(0);
+                    }
+                }
+
+            }
+            return returnPostOrt;
+        }
+    }
+}
---
+++ HospitalManagement/HospitalManagement/Patient.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagement
+{
+
+    public class Patient
+    {
+        //Properties that correspond to information about patients in the patient table.
+
+        public string Personnummer { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public int PostalCode { get; set; }
+        public string PostalArea { get; set; }
+        public string PhoneNr { get; set; }
+        public string Email { get; set; }
+        public string BloodType { get; set; }
+        
+        //Properties below commented out until the classes they refer to have been created.
+        //There are most likely more, these are just examples.
+        //public Room CurrentRoom { get; set; }
+        //public list<JournalPost> myEntries { get; set; } 
+        //public list<Prescription> myPrescriptions { get; set; }
+        //public list<Booking> myBookings { get; set; }
+
+        public Patient (string persNr, string firstName, string lastName, string address, int postCode, string postArea, string phoneNr, string eMail, string blood)
+        {
+            //Constructor with all the basic information about the patient. Will be called from a class/method
+            //that loads the information from the database and creates the patient object.
+            this.Personnummer = persNr;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Address = address;
+            this.PostalCode = postCode;
+            this.PostalArea = postArea;
+            this.Email = eMail;
+            this.BloodType = blood;
+        }
+
+    }
+}
