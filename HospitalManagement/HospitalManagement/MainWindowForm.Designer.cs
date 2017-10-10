38876c0df6985d22dca320c3832a6c01a7db5d28
Johannes Svensson <mirrorspawn@gmail.com>
2017-10-10 14:56:28 +0200

    Adds tab-window to main window.

--- HospitalManagement/HospitalManagement/MainWindowForm.Designer.cs
+++ HospitalManagement/HospitalManagement/MainWindowForm.Designer.cs
@@ -32,18 +32,13 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
             this.newPatientBtn = new System.Windows.Forms.Button();
-            this.panel4 = new System.Windows.Forms.Panel();
-            this.testPatientJournalBtn = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tableLayoutPane.SuspendLayout();
             this.panel3.SuspendLayout();
-            this.panel4.SuspendLayout();
+            this.tabControl1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPane
@@ -54,12 +49,13 @@
             this.tableLayoutPane.Controls.Add(this.panel1, 0, 0);
             this.tableLayoutPane.Controls.Add(this.panel2, 1, 0);
             this.tableLayoutPane.Controls.Add(this.panel3, 0, 1);
-            this.tableLayoutPane.Controls.Add(this.panel4, 1, 1);
+            this.tableLayoutPane.Controls.Add(this.tabControl1, 1, 1);
             this.tableLayoutPane.Location = new System.Drawing.Point(0, 1);
             this.tableLayoutPane.Name = "tableLayoutPane";
             this.tableLayoutPane.RowCount = 2;
             this.tableLayoutPane.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.602151F));
             this.tableLayoutPane.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 91.39785F));
+            this.tableLayoutPane.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPane.Size = new System.Drawing.Size(784, 462);
             this.tableLayoutPane.TabIndex = 0;
             // 
@@ -79,102 +75,50 @@
             // 
             // panel3
             // 
+            this.panel3.Controls.Add(this.button3);
             this.panel3.Controls.Add(this.newPatientBtn);
             this.panel3.Location = new System.Drawing.Point(3, 42);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(105, 417);
             this.panel3.TabIndex = 2;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(3, 32);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(99, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "PatientJournal";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // newPatientBtn
             // 
             this.newPatientBtn.Location = new System.Drawing.Point(3, 3);
             this.newPatientBtn.Name = "newPatientBtn";
-            this.newPatientBtn.Size = new System.Drawing.Size(93, 23);
+            this.newPatientBtn.Size = new System.Drawing.Size(99, 23);
             this.newPatientBtn.TabIndex = 0;
             this.newPatientBtn.Text = "Ny Patient";
             this.newPatientBtn.UseVisualStyleBackColor = true;
             this.newPatientBtn.Click += new System.EventHandler(this.newPatientBtn_Click);
             // 
-            // panel4
+            // tabControl1
             // 
-            this.panel4.Controls.Add(this.testPatientJournalBtn);
-            this.panel4.Controls.Add(this.button6);
-            this.panel4.Controls.Add(this.button5);
-            this.panel4.Controls.Add(this.button4);
-            this.panel4.Controls.Add(this.button2);
-            this.panel4.Controls.Add(this.textBox1);
-            this.panel4.Controls.Add(this.button1);
-            this.panel4.Location = new System.Drawing.Point(114, 42);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(667, 417);
-            this.panel4.TabIndex = 3;
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(114, 42);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(667, 417);
+            this.tabControl1.TabIndex = 3;
             // 
-            // testPatientJournalBtn
+            // tabPage1
             // 
-            this.testPatientJournalBtn.Location = new System.Drawing.Point(18, 186);
-            this.testPatientJournalBtn.Name = "testPatientJournalBtn";
-            this.testPatientJournalBtn.Size = new System.Drawing.Size(139, 23);
-            this.testPatientJournalBtn.TabIndex = 7;
-            this.testPatientJournalBtn.Text = "Testa PatientJournal";
-            this.testPatientJournalBtn.UseVisualStyleBackColor = true;
-            this.testPatientJournalBtn.Click += new System.EventHandler(this.testPatientJournalBtn_Click);
-            // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(288, 102);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(97, 23);
-            this.button6.TabIndex = 6;
-            this.button6.Text = "PersonExists";
-            this.button6.UseVisualStyleBackColor = true;
-            this.button6.Click += new System.EventHandler(this.button6_Click_1);
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(310, 49);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
-            this.button5.TabIndex = 5;
-            this.button5.Text = "Update";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(400, 49);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "Delete";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(217, 49);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Load/View";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(110, 106);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 1;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(110, 49);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Create";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(659, 391);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Patienter";
+            this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // MainWindow
             // 
@@ -186,8 +130,7 @@
             this.Text = "HospitalManagement";
             this.tableLayoutPane.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
-            this.panel4.ResumeLayout(false);
-            this.panel4.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -198,15 +141,10 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Panel panel4;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button newPatientBtn;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button testPatientJournalBtn;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
     }
 }
 
--- HospitalManagement/HospitalManagement/MainWindowForm.cs
+++ HospitalManagement/HospitalManagement/MainWindowForm.cs
@@ -20,52 +20,12 @@ namespace HospitalManagement
             
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            DatabaseHandler db = new DatabaseHandler();
-            Patient patientToAdd = new Patient("720524-5033", "Johannes", "Svensson", "Temperaturgatan 51", 41841, "Göteborg", "0768-406023", "none@YourBusiness.com", "B-negativ");
-            db.AddPatient(patientToAdd);
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            DatabaseHandler db = new DatabaseHandler();
-            testPatient = db.LoadPatient("720524-5033");
-            textBox1.Text = testPatient.FirstName;
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            DatabaseHandler db = new DatabaseHandler();
-            db.DeletePatient("720524-5033");
-        }
-
-        private void button5_Click(object sender, EventArgs e)
-        {
-            DatabaseHandler db = new DatabaseHandler();
-            Patient patientToUpdate = new Patient("720524-5033", "Anders", "Svensson", "Temperaturgatan 51", 41841, "Göteborg", "0768-406023", "none@YourBusiness.com", "B-negativ");
-            db.UpdatePatient(patientToUpdate);
-        }
+       
 
         private void newPatientBtn_Click(object sender, EventArgs e)
         {
             Form patReg = new PatientRegistryForm();
             patReg.Show();
-        }
-
-        private void button6_Click_1(object sender, EventArgs e)
-        {
-            DatabaseHandler db = new DatabaseHandler();
-            string answer = db.PatientExists("720524-5033").ToString();
-            textBox1.Text = answer;
-        }
-
-        private void testPatientJournalBtn_Click(object sender, EventArgs e)
-        {
-            DatabaseHandler db = new DatabaseHandler();
-            testPatient = db.LoadPatient("555555-5555");
-            Form patientJournal = new PatientJournalForm(testPatient);
-            patientJournal.Show();
         }
     }
 }
